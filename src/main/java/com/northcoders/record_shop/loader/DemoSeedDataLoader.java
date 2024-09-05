1101d83f1987dd6a637784a2b71da9d972015bda
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-05 11:54:38 +0100

    fix: rename class name to DemoSeedDataLoader to avoid name conflicts in spring boot

--- src/main/java/com/northcoders/record_shop/loader/DemoSeedDataLoader.java
+++ src/main/java/com/northcoders/record_shop/loader/DemoSeedDataLoader.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DemoDataLoader {
+public class DemoSeedDataLoader {
 
     @Bean(name = "demoDataLoader")
     public CommandLineRunner demoDataLoader(AlbumRepository albumRepository) {
