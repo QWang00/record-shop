de7c4b3765635e327eab3a8e5fcbc7108ba10a9c
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-05 14:19:57 +0100

    feat: implement getAlbumById method in service layer

--- src/main/java/com/northcoders/record_shop/service/AlbumService.java
+++ src/main/java/com/northcoders/record_shop/service/AlbumService.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface AlbumService {
     List<Album> getAllAlbums();
+
+    Album getAlbumById(Long id);
 }
--- src/main/java/com/northcoders/record_shop/service/AlbumServiceImpl.java
+++ src/main/java/com/northcoders/record_shop/service/AlbumServiceImpl.java
@@ -1,12 +1,13 @@
 package com.northcoders.record_shop.service;
 
+import com.northcoders.record_shop.exception.ItemNotFoundException;
 import com.northcoders.record_shop.model.Album;
 import com.northcoders.record_shop.repository.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AlbumServiceImpl implements AlbumService {
@@ -23,4 +24,14 @@ public class AlbumServiceImpl implements AlbumService {
         albumRepository.findAll().forEach(albums::add);
         return albums;
     }
+
+    @Override
+    public Album getAlbumById(Long id) {
+        Optional<Album> album = albumRepository.findById(id);
+        if (album.isPresent()){
+            return album.get();
+        }
+        throw new ItemNotFoundException(String.format("The album with id '%s' cannot be found", id));
+    };
+
 }
