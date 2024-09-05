582ef520887cdebea4e63ac390415cbff829c5e0
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-05 16:05:21 +0100

    test: add test for getAlbumById for when Id not existing for AlbumControllerTests class

--- src/test/java/com/northcoders/record_shop/controller/AlbumControllerTests.java
+++ src/test/java/com/northcoders/record_shop/controller/AlbumControllerTests.java
@@ -1,6 +1,8 @@
 package com.northcoders.record_shop.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.northcoders.record_shop.exception.GlobalExceptionHandler;
+import com.northcoders.record_shop.exception.ItemNotFoundException;
 import com.northcoders.record_shop.model.Album;
 import com.northcoders.record_shop.service.AlbumServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,6 +21,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -37,7 +40,9 @@ public class AlbumControllerTests {
 
     @BeforeEach
     public void setup(){
-        mockMvcController = MockMvcBuilders.standaloneSetup(albumController).build();
+        mockMvcController = MockMvcBuilders.standaloneSetup(albumController)
+                .setControllerAdvice(new GlobalExceptionHandler())
+                .build();
         mapper = new ObjectMapper();
     }
 
@@ -52,7 +57,7 @@ public class AlbumControllerTests {
         when(mockAlbumServiceImpl.getAllAlbums()).thenReturn(albums);
         this.mockMvcController.perform(
                 MockMvcRequestBuilders.get("/albums"))
-                .andExpect((MockMvcResultMatchers.status().isOk()))
+                .andExpect((status().isOk()))
                 .andExpect((MockMvcResultMatchers.jsonPath("$[0].id").value(1)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Definitely Maybe"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].artist").value("Oasis"))
@@ -70,12 +75,24 @@ public class AlbumControllerTests {
         when(mockAlbumServiceImpl.getAlbumById(1L)).thenReturn(album1);
         this.mockMvcController.perform(
                 MockMvcRequestBuilders.get("/albums/1"))
-                .andExpect((MockMvcResultMatchers.status().isOk()))
+                .andExpect((status().isOk()))
                 .andExpect((MockMvcResultMatchers.jsonPath("$.id").value(1)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Definitely Maybe"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.artist").value("Oasis"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.releaseYear").value(1994));
 
 
+    }
+
+    @Test
+    void getAlbumById_IdNotExists() throws Exception{
+        Long idNotExists = 100L;
+        String errorMessage = String.format("The album with id %d cannot be found", idNotExists);
+
+        when(mockAlbumServiceImpl.getAlbumById(idNotExists)).thenThrow(new ItemNotFoundException(errorMessage));
+
+        this.mockMvcController.perform(MockMvcRequestBuilders.get("/albums/" + idNotExists))
+                .andExpect(status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").value(errorMessage));
     }
 }
