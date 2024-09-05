34a7b41ffc056341dcb1e05aa5713844e611349f
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-05 14:03:02 +0100

    feat: create a global exception handler for all controllers

--- src/main/java/com/northcoders/record_shop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/northcoders/record_shop/exception/GlobalExceptionHandler.java
@@ -1,2 +1,15 @@
-package com.northcoders.record_shop.exception;public class GlobalExceptionHandler {
+package com.northcoders.record_shop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<Object> handleItemNotFoundException(ItemNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
 }
