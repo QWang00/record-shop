4987fb9803c65af382fa6537b6fcb82ca08784d2
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-05 14:19:16 +0100

    feat: create getAlbumById method in controller layer

--- src/main/java/com/northcoders/record_shop/controller/AlbumController.java
+++ src/main/java/com/northcoders/record_shop/controller/AlbumController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("albums")
@@ -25,6 +27,11 @@ public class AlbumController {
     public ResponseEntity<List<Album>> getAllAlums (){
         return new ResponseEntity<>(albumService.getAllAlbums(), HttpStatus.OK);
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Album> getAlbumById(@PathVariable Long id){
+        return new ResponseEntity<>(albumService.getAlbumById(id), HttpStatus.OK);
     }
 
 }
