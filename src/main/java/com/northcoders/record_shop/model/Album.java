051bb83d5bcb279ce6344ec2d0dff8fd106c35fc
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-05 11:01:34 +0100

    feat: create Album class in model package with JPA annotation & Genre enum

--- src/main/java/com/northcoders/record_shop/model/Album.java
+++ src/main/java/com/northcoders/record_shop/model/Album.java
@@ -1,2 +1,40 @@
-package com.northcoders.record_shop.model;public class Album {
+package com.northcoders.record_shop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Album {
+
+    public enum AlbumGenres {
+        BRITPOP,
+        ROCK,
+        ALTERNATIVE_ROCK
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false)
+    Long id;
+
+    @Column
+    String artist;
+
+    @Column
+    int releaseYear;
+
+    @Column
+    AlbumGenres genre;
+
+    @Column
+    String name;
 }
