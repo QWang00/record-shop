d2b7369cfc60eb08b630fdb3ba5a1e789170c5cf
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-05 12:09:13 +0100

    feat: implement logic getAllAlbums() in service layer

--- src/main/java/com/northcoders/record_shop/controller/AlbumController.java
+++ src/main/java/com/northcoders/record_shop/controller/AlbumController.java
@@ -1,9 +1,15 @@
 package com.northcoders.record_shop.controller;
 
+import com.northcoders.record_shop.model.Album;
 import com.northcoders.record_shop.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("albums")
@@ -13,6 +19,12 @@ public class AlbumController {
     @Autowired
     public AlbumController(AlbumService albumService) {
         this.albumService = albumService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Album>> getAllAlums (){
+        return new ResponseEntity<>(albumService.getAllAlbums(), HttpStatus.OK);
+
     }
 
 }
--- src/main/java/com/northcoders/record_shop/service/AlbumServiceImpl.java
+++ src/main/java/com/northcoders/record_shop/service/AlbumServiceImpl.java
@@ -1,7 +1,26 @@
 package com.northcoders.record_shop.service;
 
+import com.northcoders.record_shop.model.Album;
+import com.northcoders.record_shop.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AlbumServiceImpl implements AlbumService {
+    private final AlbumRepository albumRepository;
+
+    @Autowired
+    public AlbumServiceImpl(AlbumRepository albumRepository) {
+        this.albumRepository = albumRepository;
+    }
+
+    @Override
+    public List<Album> getAllAlbums() {
+        List<Album> albums = new ArrayList<>();
+        albumRepository.findAll().forEach(albums::add);
+        return albums;
+    }
 }
