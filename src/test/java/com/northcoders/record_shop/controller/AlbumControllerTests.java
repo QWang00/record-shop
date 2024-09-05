2fec1e3ee596bc99c3fa8c0c1ac70fb74e396926
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-05 13:49:56 +0100

    test: create tests for getAllAlbums method in controlelr layer

--- src/test/java/com/northcoders/record_shop/controller/AlbumControllerTests.java
+++ src/test/java/com/northcoders/record_shop/controller/AlbumControllerTests.java
@@ -1,4 +1,61 @@
 package com.northcoders.record_shop.controller;
 
-public class AlbumControllerTest {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.northcoders.record_shop.model.Album;
+import com.northcoders.record_shop.service.AlbumServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class AlbumControllerTests {
+
+    @Mock
+    private AlbumServiceImpl mockAlbumServiceImpl;
+
+    @InjectMocks
+    private AlbumController albumController;
+
+    @Autowired
+    private MockMvc mockMvcController;
+
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    public void setup(){
+        mockMvcController = MockMvcBuilders.standaloneSetup(albumController).build();
+        mapper = new ObjectMapper();
+    }
+
+    @Test
+    void getAllAlbums() throws Exception{
+        List<Album> albums = new ArrayList<>();
+        Album album1 = new Album(1L, "Oasis", 1994, Album.AlbumGenres.BRITPOP, "Definitely Maybe");
+        Album album2 = new Album(2L, "Oasis", 1995, Album.AlbumGenres.BRITPOP, "What's the Story Morning Glory?");
+        albums.add(album1);
+        albums.add(album2);
+
+        when(mockAlbumServiceImpl.getAllAlbums()).thenReturn(albums);
+        this.mockMvcController.perform(
+                MockMvcRequestBuilders.get("/albums"))
+                .andExpect((MockMvcResultMatchers.status().isOk()))
+                .andExpect((MockMvcResultMatchers.jsonPath("$[0].id").value(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Definitely Maybe"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("What's the Story Morning Glory?"));
+    }
 }
