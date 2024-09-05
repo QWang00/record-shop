9e0c7d1df95c07856469094660e92462fd0f1a83
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-05 14:58:10 +0100

    test: add tests for getAlbumById with Id exists & not exists in service layer

--- src/test/java/com/northcoders/record_shop/service/AlbumServiceTests.java
+++ src/test/java/com/northcoders/record_shop/service/AlbumServiceTests.java
@@ -1,5 +1,6 @@
 package com.northcoders.record_shop.service;
 
+import com.northcoders.record_shop.exception.ItemNotFoundException;
 import com.northcoders.record_shop.model.Album;
 import com.northcoders.record_shop.repository.AlbumRepository;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -26,7 +29,7 @@ public class AlbumServiceTests {
     private AlbumRepository albumRepository;
 
     @Test
-    void testGetAllAlbums(){
+    void testGetAllAlbums_ReturnsListOfAlbums(){
         List<Album> albums = new ArrayList<>();
         Album album1 = new Album(null, "Oasis", 1994, Album.AlbumGenres.BRITPOP, "Definitely Maybe");
         Album album2 = new Album(null, "Oasis", 1995, Album.AlbumGenres.BRITPOP, "What's the Story Morning Glory?");
@@ -39,6 +42,34 @@ public class AlbumServiceTests {
         assertThat(actualResult).isEqualTo(albums);
 
     }
+
+    @Test
+    void testGetAlbumById_IdNotExists(){
+        Long idNotExists = 100L;
+        when(mockAlbumRepository.findById(idNotExists)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(()-> albumServiceImpl.getAlbumById(idNotExists))
+                .isInstanceOf(ItemNotFoundException.class)
+                .hasMessage(String.format("The album with id '%s' cannot be found", idNotExists));
+
+    }
+
+    @Test
+    void testGetAlbumById_IdExists(){
+        Album album1 = new Album(1L, "Oasis", 1994, Album.AlbumGenres.BRITPOP, "Definitely Maybe");
+
+        when(mockAlbumRepository.findById(1L)).thenReturn(Optional.of(album1));
+        Album actualResult = albumServiceImpl.getAlbumById(1L);
+        assertThat(actualResult).isEqualTo(album1);
+        assertThat(actualResult.getName().equals("Definitely Maybe"));
+        assertThat(actualResult.getArtist().equals("Oasis"));
+        assertThat(actualResult.getGenre().equals(Album.AlbumGenres.BRITPOP));
+        assertThat(actualResult.getReleaseYear() == 1994);
+
+    }
+
+
+
 
 
 }
