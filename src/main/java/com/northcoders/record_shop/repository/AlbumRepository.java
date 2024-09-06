de86c3581fc5d2e6e63e77b0a8cab6cc0891736d
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-06 14:26:23 +0100

    feat: Add findByArtist method to AlbumRepository for querying albums by artist

--- src/main/java/com/northcoders/record_shop/repository/AlbumRepository.java
+++ src/main/java/com/northcoders/record_shop/repository/AlbumRepository.java
@@ -4,6 +4,9 @@ import com.northcoders.record_shop.model.Album;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AlbumRepository extends CrudRepository<Album, Long> {
+    List<Album> findByArtist(String artist);
 }
