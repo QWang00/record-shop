753248a195991811923260a7825202f158756851
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-09-07 17:50:38 +0100

    minor: add customOpenAPI bean to add title & decription for swagger endpoint in SpringDocConfig

--- src/main/java/com/northcoders/record_shop/config/SpringDocConfig.java
+++ src/main/java/com/northcoders/record_shop/config/SpringDocConfig.java
@@ -1,5 +1,7 @@
 package com.northcoders.record_shop.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +15,13 @@ public class SpringDocConfig {
                 .group("springboot-api")
                 .pathsToMatch("/**")
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Record Shop API")
+                        .description("API documentation for managing the Record Shop's album inventory. Provides endpoints for searching, adding, updating, and deleting albums, and more. Designed to support both staff operations and customer searches.")
+                        .version("1.0.0"));
     }
 }
