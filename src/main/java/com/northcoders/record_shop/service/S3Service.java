d0eb17067bd82da5457d7262c83962d5825643eb
qiuqiu <qiujuanwang87@gmail.com>
2024-09-19 17:13:50 +0100

    feat: implement S3 upload service for image handling

---
+++ src/main/java/com/northcoders/record_shop/service/S3Service.java
@@ -0,0 +1,55 @@
+package com.northcoders.record_shop.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+import software.amazon.awssdk.core.sync.RequestBody;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    public S3Service(
+            @Value("${cloud.aws.credentials.access-key}") String accessKey,
+            @Value("${cloud.aws.credentials.secret-key}") String secretKey,
+            @Value("${cloud.aws.region.static}") String region
+    ) {
+        this.s3Client = S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String key = UUID.randomUUID().toString() + "-" + file.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType(file.getContentType())
+                .build();
+
+        PutObjectResponse putObjectResponse = s3Client.putObject(putObjectRequest,
+                RequestBody.fromBytes(file.getBytes()));
+
+        if (putObjectResponse.sdkHttpResponse().isSuccessful()) {
+            return "https://" + bucketName + ".s3.amazonaws.com/" + key;
+        } else {
+            throw new IOException("Failed to upload the file to S3");
+        }
+    }
+}
